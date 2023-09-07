a0bb85a1cafc8652aabde2b002bcfa1d72afd8f8
icebear2n2 <icebear2n2@gmail.com>
2023-09-07 15:39:00 +0900

    FIX: Swagger API 추가 작성

--- src/main/java/com/icebear2n2/todayhouse/avatar/controller/AvatarController.java
+++ src/main/java/com/icebear2n2/todayhouse/avatar/controller/AvatarController.java
@@ -3,6 +3,12 @@ package com.icebear2n2.todayhouse.avatar.controller;
 import com.icebear2n2.todayhouse.avatar.service.AvatarService;
 import com.icebear2n2.todayhouse.domain.request.AvatarRequest;
 import com.icebear2n2.todayhouse.domain.response.AvatarResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,16 +17,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/avatars")
+@Tag(name = "Avatar REST API", description = "아바타 REST API")
 public class AvatarController {
 
     private final AvatarService avatarService;
 
     @PostMapping
+    @Operation(summary = "아바타 생성", description = "유저 아이디를 통해 해당 유저의 아바타 정보를 작성할 수 있는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created Avatar"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "User Not Found", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public void insert(@RequestBody AvatarRequest request) {
         avatarService.insert(request);
     }
 
     @GetMapping
+    @Operation(summary = "아바타 전체 조회", description = "아바타에 대한 전체 정보를 조회할 수 있는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully get avatars"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public Page<AvatarResponse> getAll(
             @RequestParam(name = "size", required = false, defaultValue = "5") Integer size,
             @RequestParam(name = "page", required = false, defaultValue = "0") Integer page) {
@@ -30,11 +50,25 @@ public class AvatarController {
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "아바타 정보 수정", description = "아바타에 대한 정보를 수정할 수 있는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully avatar"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Avatar Not Found", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public AvatarResponse update(@PathVariable("id") Long avatarId, @RequestBody(required = false) AvatarRequest request) {
         return avatarService.update(avatarId, request);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "아바타 삭제", description = "해당 아이디를 가진 아바타에 대한 정보를 삭제할 수 있는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully removed avatar"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Avatar Not Found", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public void delete(@PathVariable("id") Long avatarId) {
         avatarService.delete(avatarId);
     }
--- src/main/java/com/icebear2n2/todayhouse/user/controller/UserController.java
+++ src/main/java/com/icebear2n2/todayhouse/user/controller/UserController.java
@@ -7,6 +7,12 @@ import com.icebear2n2.todayhouse.domain.response.SignupResponse;
 import com.icebear2n2.todayhouse.user.config.JwtService;
 import com.icebear2n2.todayhouse.user.config.TokenInfo;
 import com.icebear2n2.todayhouse.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,11 +24,18 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Tag(name = "User REST API", description = "유저 REST API")
 public class UserController {
 
     private final UserService service;
     private final JwtService jwtService;
     @PostMapping("check")
+    @Operation(summary = "유저 정보 확인", description = "해당 유저의 회원가입 여부를 체크하는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully create user"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public SignupResponse check(
             @RequestHeader("Authorization") String token
     ){
@@ -32,16 +45,34 @@ public class UserController {
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "유저 회원가입", description = "[auth-service] Signup 메서드 요청을 통한 유저 정보 저장 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully create user"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public void save(@RequestBody UserRequest request){
         service.save(request);
     }
     @GetMapping("me")
+    @Operation(summary = "유저 정보 확인", description = "해당 유저의 정보를 조회하는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully get user information."),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public User getMe(@RequestHeader("Authorization") String token){
         TokenInfo tokenInfo = jwtService.parseToken(
                 token.replace("Bearer ", ""));
         return service.getMe(tokenInfo);
     }
     @PostMapping("signup")
+    @Operation(summary = "유저 회원가입", description = "[auth-service] Signup 메서드 요청을 통한 유저 정보 저장 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully create user"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public SignupResponse signup(
             @RequestBody SignupRequest request,
             @RequestHeader("Authorization") String token){
@@ -51,6 +82,12 @@ public class UserController {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "유저 아이디를 통한 유저 정보 확인", description = "해당 유저 아이디를 통해 유저 정보를 조회하는 메서드 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully get user information."),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(hidden = true)))
+    })
     public User getById(@PathVariable String id){
         return service.getById(UUID.fromString(id));
     }

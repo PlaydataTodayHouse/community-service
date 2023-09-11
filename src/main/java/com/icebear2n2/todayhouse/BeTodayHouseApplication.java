47cf2649babcd23efec4c956b20efe76488bacb4
icebear2n2 <icebear2n2@gmail.com>
2023-09-11 13:40:23 +0900

    REVERT!: 이전 커밋으로

--- src/main/java/com/icebear2n2/todayhouse/BeTodayHouseApplication.java
+++ src/main/java/com/icebear2n2/todayhouse/BeTodayHouseApplication.java
@@ -2,8 +2,10 @@ package com.icebear2n2.todayhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BeTodayHouseApplication {
 
     public static void main(String[] args) {
